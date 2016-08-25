9c11bf087ca3d56af5eba7e34ce8da15bbcebad7
Ethan Law and Jena SanCartier <ethan@>
2016-08-25 13:47:33 -0700

    add computer button that activates a computers turn, and disable buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,6 @@ function Player(name, roll, turnTotal, gameTotal) {
   this.name = name;
   this.roll = roll;
   this.turnTotal = turnTotal;
-  // this.addRoll = function(roll) {
-  //   if (roll === 1) {
-  //     this.turnTotal = 0;
-  //     alert("You rolled a 1! Next player's turn.");
-  //   } else {
-  //     this.turnTotal += roll;
-  //   }
-  // }
   this.gameTotal = gameTotal;
   this.winner = function() {
     if (this.gameTotal >= 100) {
@@ -46,7 +38,6 @@ var newPlayer2;
 //user interface logic:
 $(document).ready(function(){
   $("form").submit(function(event){
-    debugger;
     event.preventDefault();
     var name1 = $("#name1").val();
     var name2 = $("#name2").val();
@@ -54,6 +45,7 @@ $(document).ready(function(){
     $(".info").hide();
     $(".game").show();
     $("#submitName").hide();
+    $("#computer-turn").show();
     $("#instructionsTitle").show();
     $("h3#player1Name").text(name1);
     $("h3#player2Name").text(name2);
@@ -84,15 +76,41 @@ $(document).ready(function(){
 
   $("#player1-hold").click(function(){
     newPlayer1.hold();
+    $("#player1-hold, #player1-roll").prop('disabled', true);
+    $("#player2-hold, #player2-roll").prop('disabled', false);
     $("#p1TurnTotal").text("Turn: " + newPlayer1.turnTotal);
     $("#p1RollTotal").text("Roll: " + newPlayer1.roll);
     $("#p1GameTotal").text("Game total: " + newPlayer1.gameTotal);
+
   });
 
   $("#player2-hold").click(function(){
     newPlayer2.hold();
+    $("#player2-hold, #player2-roll").prop('disabled', true);
+    $("#player1-hold, #player1-roll").prop('disabled', false);
     $("#p2TurnTotal").text("Turn: " + newPlayer2.turnTotal);
     $("#p2RollTotal").text("Roll: " + newPlayer2.roll);
+    $("#p2GameTotal").text("Game total: " + newPlayer2.gameTotal);
+  });
+
+  $("#computer-turn").click(function(){
+debugger;
+    while (newPlayer2.roll !== 1) {
+      if (newPlayer2.gameTotal + newPlayer2.turnTotal >= 100) {
+        newPlayer2.hold();
+        break;
+      } else if (newPlayer2.turnTotal >= 10) {
+        newPlayer2.hold();
+        break;
+      } else {
+        newPlayer2.rollRandom();
+        newPlayer2.addRoll(newPlayer2.roll);
+      }
+      console.log("Roll: " + newPlayer2.roll + " TT: " + newPlayer2.turnTotal);
+    }
+
+    $("#p2RollTotal").text("Roll: " + newPlayer2.roll);
+    $("#p2TurnTotal").text("Turn: " + newPlayer2.turnTotal);
     $("#p2GameTotal").text("Game total: " + newPlayer2.gameTotal);
   });
 
